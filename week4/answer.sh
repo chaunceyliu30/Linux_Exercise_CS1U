041c184beb90195bbbb6447888da833d13f3f0a1
Chenxu Liu <cxchenxu.liu@mail.utoronto.ca>
2022-01-17 18:07:42 +0800

    first commit

---
+++ week4/answer.sh
@@ -0,0 +1,14 @@
+########## numbers1 - Search for numbers with dashes (ie, 123-456-7890) ##########
+grep '[0-9]{3}-[0-9]{3}-[0-9]{4}' cs1u* golden > output
+
+########## numbers2 - Search for numbers with dashes and those with no dashes (ie, 1234567890) ##########
+grep '[0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}' cs1u* golden > output
+
+########## numbers3 - Search for numbers with dashes, no dashes, and those with parens (ie, (123)456-7890) ##########
+grep '[(]?[0-9]{3}[)]?[-]?[0-9]{3}[-]?[0-9]{4}' cs1u* golden > output
+
+########## numbers4 - Again, you want dashes, no dashes, parens.  But someone malicious introduced malformed output into our files: missing parens (ie, 123456-7890 or 123)456-7890) -- so you SHOULD NOT match malformed numbers. ##########
+grep '\([0-9]{3}\)[0-9]{3}-[0-9]{4}|[^(][0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}' cs1u* golden > output
+
+########## numbers4 - Again, you want dashes, no dashes, parens.  But someone malicious introduced malformed output into our files: missing parens (ie, 123456-7890 or 123)456-7890) -- so you SHOULD NOT match malformed numbers. ##########
+grep '[^(][0-9]{10}|\([0-9]{3}\)[0-9]{3}[-][0-9]{4}|[^(][0-9]{3}[-][0-9]{3}[-][0-9]{4}' cs1u* golden > output
