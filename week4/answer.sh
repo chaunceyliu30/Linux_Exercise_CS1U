1e2846ec6bd646c9c1d4b054bb26aea601eeefc4
Chenxu Liu <cxchenxu.liu@mail.utoronto.ca>
2022-01-17 18:09:04 +0800

    corrected comment

--- week4/answer.sh
+++ week4/answer.sh
@@ -10,5 +10,5 @@ grep '[(]?[0-9]{3}[)]?[-]?[0-9]{3}[-]?[0-9]{4}' cs1u* golden > output
 ########## numbers4 - Again, you want dashes, no dashes, parens.  But someone malicious introduced malformed output into our files: missing parens (ie, 123456-7890 or 123)456-7890) -- so you SHOULD NOT match malformed numbers. ##########
 grep '\([0-9]{3}\)[0-9]{3}-[0-9]{4}|[^(][0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}' cs1u* golden > output
 
-########## numbers4 - Again, you want dashes, no dashes, parens.  But someone malicious introduced malformed output into our files: missing parens (ie, 123456-7890 or 123)456-7890) -- so you SHOULD NOT match malformed numbers. ##########
+########## numbers5 - Dashes, no dashes, parens.  Now you have missing parens and missing dashes (ie, 123-4567890) as part of the malformations-- again you SHOULD NOT match malformed numbers. ##########
 grep '[^(][0-9]{10}|\([0-9]{3}\)[0-9]{3}[-][0-9]{4}|[^(][0-9]{3}[-][0-9]{3}[-][0-9]{4}' cs1u* golden > output
